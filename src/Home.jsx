b272a00b2e181823b5c8925005cb908e65bcd74e
lachierigg05 <lachrigg@gmail.com>
2024-09-02 11:28:00 +1000

    redesign of home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,23 @@
 function Home() {
-    return (
-       <div id="homeTextGroup">
-            {/* <h3 id="homeText">THIS IS</h3> */}
-            <h1 id="trueBloodsHome">TRUE BLOODS</h1>
-            <h3 id="homeText">THE #1 INDEPENDENT SYDNEY SWANS FAN PODCAST</h3>
-            <div id="imageContainer">
-                {/* <img src="src/assets/429531389_18252483556243094_2699257026716413981_n.jpg" id="proPic"></img> */}
-                {/* <img src="src/assets/346471664_785012463151378_1603579096252969499_n.jpg" id="logoHome"></img> */}
-                {/* <img src="src/assets/338347760_758649059061410_771832629323742547_n.jpg" id="jumperPic"></img> */}
+  return (
+    <div id="homeContainer">
+        {/* <div id="homeSectionRight">
+            <div id="logoContainer">
+                <img src='src/assets/Logo/TrueBloods_CMYK_Master.svg' id='logo' alt='Logo'></img>
+            </div>
+        </div> */}
+        <div id="homeSectionLeft">
+            <div id="homeTextContainer">
+                <h3>THIS IS</h3>
+                <h1 id="truebloods">TRUE BLOODS</h1>
+                <h3>THE #1 INDEPENDENT SYDNEY SWANS FAN PODCAST</h3>
+                <div id="logoContainer">
+                    <img src='src/assets/Logo/TrueBloods_CMYK_Master.svg' id='logo' alt='Logo'></img>
+                </div>
             </div>
         </div>
-    );
+    </div>
+  );
 }
 
 export default Home;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
         <nav id="topnavbar">
-            <img src='src/assets/Logo/TrueBloods_CMYK_Master.svg' id='logoNavbar' alt='Logo'></img>
             <ul id="linklist">
                 <Link to="/">HOME</Link>
                 <Link to="/about">ABOUT</Link>
@@ -11,6 +10,23 @@ function Navbar() {
                 <Link to="/listen">LISTEN</Link>
                 <Link to="/contact">CONTACT</Link>
             </ul>
+            <div id="logoList"> 
+                <a href="https://open.spotify.com/show/37RysKUkNmedH0fI1TPYDl">
+                    <img
+                    src="src/assets/Spotify_Primary_Logo_RGB_Black.png"
+                    alt="SpotifyLogo"
+                    className="logoListLogo"
+                    id="spotifyLogo"
+                    />
+                </a>
+                <a href="https://www.facebook.com/TrueBloodsPod">
+                    <img
+                    src="src/assets/icons8-facebook-500.png"
+                    alt="FacebookLogo"
+                    className="logoListLogo"
+                    />
+                </a>
+            </div>   
         </nav>
     )
 }
